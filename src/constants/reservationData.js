cf08215eef9a1d7fe05395b408f8b5e0b6739196
Cezar Ionut Popa <cezar.popa@essensys.ro>
2023-12-05 14:35:05 +0200

    initial commit

---
+++ src/constants/reservationData.js
@@ -0,0 +1,10 @@
+import MarioandAdrianA from '../assets/MarioandAdrianA.jpg';
+  
+  const reservationData = {
+        title: "Reserve",
+        location: "A Table",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab tempora deleniti eum repellat explicabo ratione quisquam fugiat quidem..",
+        image: MarioandAdrianA
+    }
+
+export default reservationData;
